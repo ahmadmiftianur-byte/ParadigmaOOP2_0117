881048c4de14c370e50511a3ff9ce01699296f2e
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-05-19 19:05:38 +0700

    mmebuat 3 klass dan membuat polimorti di dalam nya

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class seseorang {
+public:
+    //virtual void pesan() = 0;
+    virtual void pesan() {
+        cout << "Pesan dari seseorang." << endl;
+    }
+};
+
+class joko : public seseorang {
+public:
+    void pesan() override {
+        cout << "Pesan dari Joko." << endl;
+    }
+};
+
+class lia : public seseorang {
+public: 
+    void pesan() {
+        cout << "Pesan dari Lia." << endl;
+    }
+};
+
